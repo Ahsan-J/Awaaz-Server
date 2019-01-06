e7499a938da9b4f98d37fd7c1355fa3ce7477fc7
Ahsan-J <ahsan.ahmed99@ymail.com>
2019-01-07 02:33:21 +0500

    Created a Constant to handle basic general configurations

---
+++ helpers/constants.go
@@ -0,0 +1,29 @@
+package helpers
+
+// DateLayout is the standard layout used throughout the program for Date only
+var DateLayout = "2006-01-02"
+
+// DateTimeLayout is the standard layout used throughout the program for DateTime
+var DateTimeLayout = ""
+
+// TimeLayout is the standard layout used throughout the program for Time only
+var TimeLayout = "03:04 PM"
+
+var accessTokenTime = 900       // 15 minutes in seconds
+var refreshTokenTime = 15780000 // 6 months in seconds
+
+// Status to determine data validity
+var approved = 1
+var pending = 2
+var blocked = 4
+var anonymous = 8
+var admin = 16
+var superAdmin = 32
+
+var secretkey = "YXdhYXotdGhlLXJlcG9ydGluZy1hcHBsaWNhdGlvbi4="
+
+// Validation regex patterns
+var emailPattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+
+// Minimum six characters:
+var passwordPattern = "([a-zA-Z0-9]+){6,}"
