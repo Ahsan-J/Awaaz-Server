f8b43284f3f7dd2b47771abbebc5bd168715c778
Ahsan-J <ahsan.ahmed99@ymail.com>
2019-01-07 02:37:47 +0500

    Index : imported and called general routes Api

--- routes/index.go
+++ routes/index.go
@@ -1,32 +1,36 @@
 package routes
 
 import (
-	"github.com/gorilla/mux"
-	"./user"
+	"html/template"
+	"net/http"
+
 	"./admin"
 	"./attachment"
-	"net/http"
-	"html/template"
+	"./generalapi"
+	"./user"
+	"github.com/gorilla/mux"
 )
+
 // GenerateRoutes combines all of the functions and generate them to be used
-func GenerateRoutes (router * mux.Router) {
+func GenerateRoutes(router *mux.Router) {
 	admin.GenerateAdminRoutes(router)
 	// Api
+	generalapi.GenerateGeneralAPIRoutes(router)
 	user.GenerateUserAPIRoutes(router)
 	attachment.GenerateAttachmentApiRoutes(router)
-	
-	serveStatic(router);
-	router.HandleFunc("/",index);
+
+	serveStatic(router)
+	router.HandleFunc("/", index)
 }
 
-func serveStatic (router *mux.Router) {
+func serveStatic(router *mux.Router) {
 	router.PathPrefix("/uploads/").Handler(http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
 	router.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("./html/public"))))
 }
 
-func index (w http.ResponseWriter, r *http.Request){
+func index(w http.ResponseWriter, r *http.Request) {
 	// Redirects to index.html inside the html folder
 	template.
-	Must(template.ParseFiles("./html/index.html")).
-	Execute(w,nil);
+		Must(template.ParseFiles("./html/index.html")).
+		Execute(w, nil)
 }
